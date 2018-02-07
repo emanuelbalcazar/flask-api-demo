2e54e361112ff61b2409465089ee60f76c925dcd
emanuelbalcazar <emanuelbalcazar13@gmail.com>
2018-02-07 09:35:12 -0300

    Probando  la declaracion de rutas

--- api.py
+++ api.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def index():
     return 'Welcome!'
 
-@app.route('/api/info', methods=['GET'])
+@app.route('/api/info', methods = ['GET'])
 def info():
     return jsonify({ 'name': 'flask api demo', 'version': '1.0.0' })
 
+@app.route('/api/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True)
